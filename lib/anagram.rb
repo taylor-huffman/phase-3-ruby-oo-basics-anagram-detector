ef294c7f277840565aeec23ee0eb34908a4dec52
Taylor Huffman <taylor@crucialartists.com>
2022-05-06 13:16:54 -0500

    commit tree 53b380565d4279e103afbab3779fd6ea4920fdcb

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        # binding.pry
+        matches = array.select do |w|
+            w.chars.sort_by(&:downcase).join == @word.chars.sort_by(&:downcase).join
+        end
+    end
+
+end
